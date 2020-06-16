fcc76ac11878d04e4d9bd8a3ab9ece47bd6b3850
Jabbar <multimediary@gmail.com>
2020-06-16 20:20:18 +0700

    Change Header

--- application/views/v_pengguna_header.php
+++ application/views/v_pengguna_header.php
@@ -22,19 +22,6 @@
 	<nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             <a class="navbar-brand" href="#">Menu</a><button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button
             ><!-- Navbar Search-->
-            <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
-            </form>
-            <!-- Navbar-->
-            <ul class="navbar-nav ml-auto ml-md-0">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
-                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-                        <a class="dropdown-item" href="#"></a><a class="dropdown-item" href="#"></a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="<?php echo base_url(); ?>login/logout">Logout</a>
-                    </div>
-                </li>
-            </ul>
         </nav>
 <?php
   $geturl1 = $this->uri->segment(1);
@@ -60,6 +47,9 @@
 							<a class="nav-link <?php echo $user;?>" href="<?php echo base_url(); ?>user"
                                 ><div class="sb-nav-link-icon"><i class="fas fa-user"></i></div>
                                 User</a>
+							<a class="nav-link <?php echo $user;?>" href="<?php echo base_url(); ?>login/logout"
+                                ><div class="sb-nav-link-icon"><i class="fas fa-sign-out"></i></div>
+                                Logout</a>
                         </div>
                     </div>
                     <div class="sb-sidenav-footer">
